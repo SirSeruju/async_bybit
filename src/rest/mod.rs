6fea6d4d8e881fab2dfe2e2c8a4b81586f1700b9
SirSeruju <>
2024-06-20 21:37:03 +0000

    Add Get Instruments Info request

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -10,7 +10,8 @@ use crate::{
     Credentials,
 };
 
-use self::model::{CancelOrderRequest, CancelOrderResponse, Response};
+use self::model::{CancelAllOrderRequest, CancelOrderRequest, InstrumentsInfoRequest};
+use self::model::{CancelAllOrderResponse, CancelOrderResponse, InstrumentsInfoResponse, Response};
 
 pub enum Params<T> {
     Query(T),
@@ -39,16 +40,68 @@ impl Client {
 
     pub async fn cancel_order(
         &self,
-        order: CancelOrderRequest,
+        request: CancelOrderRequest,
         recv_window: u64,
     ) -> Result<Response<CancelOrderResponse>, reqwest::Error> {
         self.request_signed(
             "/v5/order/cancel".to_string(),
             Method::POST,
             recv_window,
-            Some(Params::Body(order)),
+            Some(Params::Body(request)),
         )
         .await
+    }
+
+    pub async fn cancel_all_order(
+        &self,
+        request: CancelAllOrderRequest,
+        recv_window: u64,
+    ) -> Result<Response<CancelAllOrderResponse>, reqwest::Error> {
+        self.request_signed(
+            "/v5/order/cancel-all".to_string(),
+            Method::POST,
+            recv_window,
+            Some(Params::Body(request)),
+        )
+        .await
+    }
+
+    pub async fn get_instruments_info(
+        &self,
+        request: InstrumentsInfoRequest,
+    ) -> Result<Response<InstrumentsInfoResponse>, reqwest::Error> {
+        self.request(
+            "/v5/market/instruments-info".to_string(),
+            Method::GET,
+            Some(Params::Query(request)),
+        )
+        .await
+    }
+
+    async fn request<P: Serialize, R: DeserializeOwned>(
+        &self,
+        endpoint: String,
+        method: Method,
+        params: Option<Params<P>>,
+    ) -> Result<R, reqwest::Error> {
+        let url = format!("{}{}", self.host, endpoint);
+
+        let builder = self.inner.request(method, url);
+
+        let request = match params {
+            Some(Params::Body(b)) => {
+                let msg = serde_json::to_string(&b).unwrap();
+                builder.body(msg).build()?
+            }
+            Some(Params::Query(q)) => {
+                let msg = serde_qs::to_string(&q).unwrap();
+                let mut req = builder.build()?;
+                req.url_mut().set_query(Some(&msg));
+                req
+            }
+            None => builder.build()?,
+        };
+        self.inner.execute(request).await?.json::<R>().await
     }
 
     async fn request_signed<P: Serialize, R: DeserializeOwned>(
--- src/rest/model.rs
+++ src/rest/model.rs
@@ -70,3 +70,72 @@ pub struct CancelAllOrderResponse {
     pub list: Vec<CancelOrderResponse>,
     pub success: String,
 }
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct InstrumentsInfoRequest {
+    pub category: String,
+    pub symbol: Option<String>,
+    pub status: Option<String>,
+    pub base_coin: Option<String>,
+    pub limit: Option<u64>,
+    pub cursor: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct InstrumentsInfoResponse {
+    pub category: String,
+    pub list: Vec<InstrumentInfo>,
+    pub next_page_cursor: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct InstrumentInfo {
+    pub symbol: String,
+    pub contract_type: String,
+    pub status: String,
+    pub base_coin: String,
+    pub quote_coin: String,
+    pub launch_time: String,
+    pub delivery_time: String,
+    pub delivery_fee_rate: String,
+    pub price_scale: String,
+    pub leverage_filter: LeverageFilter,
+    pub price_filter: PriceFilter,
+    pub lot_size_filter: LotSizeFilter,
+    pub unified_margin_trade: bool,
+    pub funding_interval: u64,
+    pub settle_coin: String,
+    pub copy_trading: String,
+    pub upper_funding_rate: String,
+    pub lower_funding_rate: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct LeverageFilter {
+    pub min_leverage: String,
+    pub max_leverage: String,
+    pub leverage_step: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct PriceFilter {
+    pub min_price: String,
+    pub max_price: String,
+    pub tick_size: String,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct LotSizeFilter {
+    pub max_order_qty: String,
+    pub max_mkt_order_qty: String,
+    pub min_order_qty: String,
+    pub qty_step: String,
+    pub post_only_max_order_qty: String,
+    pub min_notional_value: String,
+}
